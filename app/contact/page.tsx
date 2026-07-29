32a5886e76b982ec024c2edf22017338954e8c3f
sethbaldridge87 <sethbaldridge87@gmail.com>
2026-07-29 11:57:43 -0600

    Contact page updated to disable send button while message is being sent. Appropriate stylings also added.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,32 @@
 "use client"
 
-import { SubmitEvent  } from "react";
+import { SubmitEvent, useState } from "react";
 import styles from "./page.module.css";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Contact() {
-    async function handleOnSubmit(event: SubmitEvent <HTMLFormElement>) {
+    const router = useRouter();
+    const [messageSending, setMessageSending] = useState(false);
+    async function handleOnSubmit(event: SubmitEvent<HTMLFormElement>) {
         event.preventDefault();
-        const formData = new FormData(event.currentTarget)
 
-        const sendMail = await fetch('/api/email', {
-            method: 'POST',
-            body: JSON.stringify({
-                name: String(formData.get('userName') || ''),
-                email: String(formData.get('userEmail') || ''),
-                message: String(formData.get('userMessage') || ''),
-            })
-        });
-        console.log(sendMail.status);
-        if (sendMail.status === 200) {
-            redirect('/thankyou'); 
+        try {
+            const formData = new FormData(event.currentTarget);
+            setMessageSending(true);
+            await fetch('/api/email', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name: String(formData.get('userName') || ''),
+                    email: String(formData.get('userEmail') || ''),
+                    message: String(formData.get('userMessage') || ''),
+                }),
+            });
+            router.push("/thankyou");
+            
+        } catch (error) {
+            console.error('Failed to send email:', error);
         }
-    };
+    }
     return (
         <section className={styles.contact}>
             <h1>Contact Me</h1>
@@ -47,7 +52,7 @@ export default function Contact() {
                 <label htmlFor="userMessage">Message</label><br />
                 <textarea rows={12} name="userMessage" id="userMessage" required></textarea>
                 <br /><br />
-                <button type="submit">Send</button>
+                <button type="submit" disabled={messageSending}>{messageSending ? "Sending Message..." : "Send"}</button>
             </form>
         </section>
     );
