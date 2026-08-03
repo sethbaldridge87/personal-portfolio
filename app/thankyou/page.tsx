9208aec74b955733ab2ed97f896864d6726dfa17
sethbaldridge87 <sethbaldridge87@gmail.com>
2026-08-03 16:41:50 -0600

    Want to give this thank you page the same height as the contact page.

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -1,6 +1,6 @@
 export default function Projects() {
     return (
-        <section>
+        <section style={{ minHeight: '75vh' }}>
             <h1>Thank you for your message!</h1>
             <p>I'll get back to you as soon as I can!</p>
         </section>
