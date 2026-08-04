db70922541e0225a8d470a1dbf3c57f44c74ba9d
sethbaldridge87 <sethbaldridge87@gmail.com>
2026-08-04 12:02:00 -0600

    Returning the S logo to its original place.
    Removing these SVG files.
    It's better to render them as SVG elements so they can inherit CSS properties easily.
    Instead of changing the rendered SVG with different themes, both themes will use the same SVG, but color it differently depending on the theme.
    Adding a new bulb component which will render the SVG to avoid needlessly copying the SVG element code.

--- app/components/header/lightswitch/lightswitch.tsx
+++ app/components/header/lightswitch/lightswitch.tsx
@@ -2,10 +2,8 @@
 
 import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
-import Image from 'next/image';
-import BulbRegular from '@/public/icons/bulb-regular.svg';
-import BulbSolid from '@/public/icons/bulb-solid.svg';
 import styles from "./lightswitch.module.css";
+import Bulb from './bulb';
 
 export default function LightSwitch() {
   const [mounted, setMounted] = useState(false);
@@ -15,29 +13,30 @@ export default function LightSwitch() {
 
   if (!mounted) return (
     <button className={styles.switch}>
-      <Image
-        src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-        width={45}
-        height={50}
-        alt="Loading Light/Dark Toggle"
-        priority={false}
-        title="Loading Light/Dark Toggle"
-      />
+      <Bulb />
     </button>
   )
 
   if (resolvedTheme === 'dark') {
     return (
-      <button className={styles.switch} onClick={() => setTheme('light')} aria-pressed={mounted} aria-label={mounted ? 'Switch to light mode' : 'Switch to dark mode'}>
-        <Image height={50} width={45} src={BulbRegular} alt="Switch to light mode"></Image>
+      <button 
+      className={styles.switch} 
+      onClick={() => setTheme('light')} 
+      aria-pressed={mounted} 
+      aria-label={resolvedTheme === 'dark' ? 'Switch to light mode' : ''}>
+        <Bulb />
       </button>
     )
   }
 
   if (resolvedTheme === 'light') {
     return (
-      <button className={styles.switch} onClick={() => setTheme('dark')} aria-pressed={mounted} aria-label={mounted ? 'Switch to light mode' : 'Switch to dark mode'}>
-        <Image height={50} width={45} src={BulbSolid} alt="Switch to dark mode"></Image>
+      <button 
+      className={styles.switch} 
+      onClick={() => setTheme('dark')} 
+      aria-pressed={mounted} 
+      aria-label={resolvedTheme === 'light' ? 'Switch to dark mode' : ''}>
+        <Bulb />
       </button>
     )
   }
